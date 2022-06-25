9e8f9195e15374c2ef1aecd1f273ef5f15898e9b
choi <chlfkrgnl@naver.com>
2022-06-25 09:00:17 +0900

    init

---
+++ src/main/java/com/example/finalPJ/FinalPjApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalPJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalPjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalPjApplication.class, args);
+	}
+
+}
